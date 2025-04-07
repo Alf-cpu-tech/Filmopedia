1cac27a6303852aa73c26e397970942dcf07115a
Alf-cpu-tech <alfieridgway31@gmail.com>
2025-04-07 21:13:19 +0100

    Nav bar flexbox and grid layout
    made tweaks to the nav bar to make it scroll with the page and created basic grid layout for desktop and used a media query to switch to flexbox when browser is smaller or on mobile.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //script.js
+// taken from w3schoosl how to create a responisve nav bar
+//https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
